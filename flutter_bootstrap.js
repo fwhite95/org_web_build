d1417619006af637b8f45b5c598bcdf5634ba3ca
Frederick White <white.freddie95@gmail.com>
2024-06-27 20:21:13 -0700

    new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"be7db94196fee940e319363f7ed0c486a780ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2120521068"
+    serviceWorkerVersion: "191702574"
   }
 });
